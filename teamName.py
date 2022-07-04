0beff852b86b7853149feb8220812248a673fb15
3bobchen <83627389+3bobchen@users.noreply.github.com>
2022-07-04 17:31:48 +1000

    Minor modification to codebase to suggest where teams can write their code.

--- teamName.py
+++ teamName.py
@@ -6,9 +6,9 @@ currentPos = np.zeros(nInst)
 
 def getMyPosition (prcSoFar):
     global currentPos
-    (nins,nt) = prcSoFar.shape
-    rpos = np.array([int(x) for x in 1000 * np.random.randn(nins)])
-    currentPos += rpos
+
+    # Build your function body here
+
     return currentPos
 
     
